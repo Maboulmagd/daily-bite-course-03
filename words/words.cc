239ea8fa23f71a06ac6e02151d62337d1e896f44
Mohammed (Mo) Aboulmagd <Mohamedrefat007@hotmail.com>
2023-04-22 03:17:58 +0000

    Adding my own implementation for words (no lib)

--- words/words.cc
+++ words/words.cc
@@ -5,7 +5,27 @@
 
 std::vector<std::string> to_words(std::string text) {
 
-  std::istringstream iss(text);
-  return std::vector<std::string>{std::istream_iterator<std::string>{iss},
-                                  std::istream_iterator<std::string>()};
+  // Library solution:
+
+  // std::istringstream iss(text);
+  // return std::vector<std::string>{std::istream_iterator<std::string>{iss},
+  //                                 std::istream_iterator<std::string>()};
+
+  // My own implementation
+
+  std::vector<std::string> words;
+  std::string word;
+
+  for (const char &c : text) {
+    if (c == ' ') {
+      words.push_back(word);
+      word.clear();
+    }
+
+    else {
+      word += c;
+    }
+  }
+
+  return words;
 }
